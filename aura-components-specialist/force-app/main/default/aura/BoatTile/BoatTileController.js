cd8c8e1324629b5aca68446490b8b64ede16aadd
kirill.terentyev <kirill.terentyev@synebo.io>
2020-09-30 17:15:51 +0300

    Challenge #6: Display boat details.

--- aura-components-specialist/force-app/main/default/aura/BoatTile/BoatTileController.js
+++ aura-components-specialist/force-app/main/default/aura/BoatTile/BoatTileController.js
@@ -1,16 +1,24 @@
 ({
     doInit : function(component, event, helper) {
-        var data = component.get("v.boat");
-        console.log("data received in boattile: " + data);
+        var data=component.get("v.boat");
+        console.log("data received in boattile"+data);
     },
-
+   
     onBoatClick : function(component, event, helper) {
-        var data = component.get("v.boat");
-        console.log("data received in boattilecontroller: " + data);
-        var BoatSelect = component.getEvent("BoatSelect");
+        var data=event.getSource().get("v.value")
+        var boatid=data.Id;
+        console.log("data received in boattilecontroller"+data);
+        var BoatSelect=component.getEvent("BoatSelect");
         BoatSelect.setParams({
-            "boatId" : data
+            "boatId":boatid
         });
         BoatSelect.fire();
-    }
+       
+        //fire BoatSelected event
+        var boatselected = $A.get("e.c:BoatSelected");
+        boatselected.setParams({
+            "boat" : data
+        });
+        boatselected.fire();
+    },
 })
