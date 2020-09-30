617cc8d97467fee0bf467e81cdd90e61ce35f7c4
kirill.terentyev <kirill.terentyev@synebo.io>
2020-09-30 16:35:26 +0300

    Challenge #5: Highlight the selected boat.

--- aura-components-specialist/force-app/main/default/aura/BoatSearchResults/BoatSearchResultsController.js
+++ aura-components-specialist/force-app/main/default/aura/BoatSearchResults/BoatSearchResultsController.js
@@ -3,5 +3,10 @@
         var params = event.getParam("arguments");
         component.set("v.boatTypeId", params.boatTypeId);
         helper.onSearch(component);
+    },
+
+    onBoatSelect : function(component, event, helper) {
+        var selectedBoatId = event.getParam("boatId");
+        component.set("v.selectedBoatId", selectedBoatId);
     }
 })
--- aura-components-specialist/force-app/main/default/aura/BoatTile/BoatTileController.js
+++ aura-components-specialist/force-app/main/default/aura/BoatTile/BoatTileController.js
@@ -2,5 +2,15 @@
     doInit : function(component, event, helper) {
         var data = component.get("v.boat");
         console.log("data received in boattile: " + data);
+    },
+
+    onBoatClick : function(component, event, helper) {
+        var data = component.get("v.boat");
+        console.log("data received in boattilecontroller: " + data);
+        var BoatSelect = component.getEvent("BoatSelect");
+        BoatSelect.setParams({
+            "boatId" : data
+        });
+        BoatSelect.fire();
     }
 })
